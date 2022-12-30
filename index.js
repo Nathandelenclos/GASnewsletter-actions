be31f3f30c106a84b2901c46800495ab8722e29d
Nather_ / Nathan Delenclos <nathan.delenclos@epitech.eu>
2022-12-30 23:37:00 +0100

    [+] formData method

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const request = require('request');
 
 try {
-  const base_url = core.getInput("base_url")
-  const title = core.getInput("title")
-  const subtitle = core.getInput("subtitle")
-  const img = core.getInput("img")
-  const updateTitle = core.getInput("update_title")
-  const password = core.getInput("password")
-  const link = core.getInput("link")
-  var options = {
+  const options = {
     'method': 'POST',
-    'url': `${base_url}?title=${title}&subtitle=${subtitle}&img=${img}&updateTitle=${updateTitle}&link=${link}&time=${(new Date()).toTimeString()}&password=${password}`,
+    'url': core.getInput("base_url"),
     'headers': {
+    },
+    formData: {
+      'title': core.getInput("title"),
+      'subtitle': core.getInput("subtitle"),
+      'img': core.getInput("img"),
+      'updateTitle': core.getInput("update_title"),
+      'link': core.getInput("link"),
+      'time': (new Date()).toTimeString(),
+      'password': core.getInput("password")
     }
   };
   request(options, function (error, response) {
