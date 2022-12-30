433ae2247e0292ba0fefad3939ead49e94bc4b78
Nather_ / Nathan Delenclos <nathan.delenclos@epitech.eu>
2022-12-30 23:00:06 +0100

    [+] make request in index

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const request = require('request');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const base_url = core.getInput("base_url")
+  const title = core.getInput("title")
+  const subtitle = core.getInput("subtitle")
+  const img = core.getInput("img")
+  const updateTitle = core.getInput("update_title")
+  const password = core.getInput("password")
+  const link = core.getInput("link")
+  var options = {
+    'method': 'POST',
+    'url': `${base_url}?title=${title}&subtitle=${subtitle}&img=${img}&updateTitle=${updateTitle}&link=${link}&time=${(new Date()).toTimeString()}&password=${password}`,
+    'headers': {
+    }
+  };
+  request(options, function (error, response) {
+    if (error) throw new Error(error);
+    core.setOutput("body", response.body);
+  });
 } catch (error) {
   core.setFailed(error.message);
 }
